a0856cb8c5b9bf158b594a43c6488b828daea795
triciasykes <tlsykes98@gmail.com>
2023-09-14 14:45:41 -0700

    updating to skipping form authenticity token in login/signup

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -4,10 +4,32 @@ class ApartmentsController < ApplicationController
     render json: apartments
   end
 
+  def show
+    apartment = Apartment.find_by(params[:id])
+
   def create 
     apartment = Apartment.create(apartment_params)
     if apartment.valid?
       render json: apartment 
+    else
+      render json: apartment.errors, status: 422
+    end
+  end
+
+  def update
+    apartment = Apartment.find(params[:id])
+    apartment.update(apartment_params)
+    if apartment.valid?
+      render json: apartment
+    else
+      render json: apartment.errors, status: 422
+    end
+  end
+
+  def destroy
+    apartment = Apartment.find(params[:id])
+    if apartment.destroy
+      render json: apartments
     else
       render json: apartment.errors, status: 422
     end
